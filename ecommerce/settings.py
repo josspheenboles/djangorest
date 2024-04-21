7785dde07a3212f19abb24d22116b00d071e91b7
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-21 19:37:00 +0200

    add apps in insalled apps

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book',
+    'author.apps.AuthorConfig'
 ]
 
 MIDDLEWARE = [
