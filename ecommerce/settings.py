6cea0ddbb833303fab464060be65935a2fa73e2f
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-21 19:56:08 +0200

    mysql db configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'ecommerce',
+        'USER':'root',
+        'HOST':'localhost',
+        'PASSWORD':'',
+        'PORT':3306
     }
 }
 
