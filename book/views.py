4a63aa2eae6d9c5688177967d775b6d53b2ebdac
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-21 21:20:55 +0200

    book add

--- book/views.py
+++ book/views.py
@@ -16,7 +16,8 @@ def book_add(request):
     # pythonbook=Book(name='python bible',latest_version=2)
     # pythonbook.save()
     # javabook=Book.objects.create(name='java bible')
-    return HttpResponse('<h1>book_add</h1>')
+    # return HttpResponse('<h1>book_add</h1>')
+    return render(request,'book/add.html')
 def book_update(request,id):
     return HttpResponse(f'<h1>book_update {id}</h1>')
 def book_delete(request,id):
