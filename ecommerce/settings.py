59af19d0931deb339320e293bae63bda5469fed4
josspheenboles <eng.jospheenboles@gmail.com>
2024-05-24 06:20:43 +0300

    configure auth

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,6 +41,16 @@ INSTALLED_APPS = [
     'author.apps.AuthorConfig',
     'rest_framework',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
