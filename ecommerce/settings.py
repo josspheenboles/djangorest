b1af8ba0c88450f38b44643beb8f8aa838b9a67a
josspheenboles <eng.jospheenboles@gmail.com>
2024-05-24 06:37:49 +0300

    develope custome middleware

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'author.middleware.SimpleMiddleware',  # Add your custom middleware here
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
